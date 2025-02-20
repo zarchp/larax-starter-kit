cb13f3c0ff4c0bf7d32d44e32ee7ba9a213f14e7
Hugo Sainte-Marie <hugo.saintemarie@gmail.com>
2025-02-20 19:53:39 +0100

    Tweak register page copy

--- resources/js/pages/auth/register.tsx
+++ resources/js/pages/auth/register.tsx
@@ -32,7 +32,7 @@ export default function Register() {
     };
 
     return (
-        <AuthLayout title="Create an account" description="Enter your information below to create your account">
+        <AuthLayout title="Create an account" description="Enter your details below to create your account">
             <Head title="Register" />
             <form className="flex flex-col gap-6" onSubmit={submit}>
                 <div className="grid gap-6">
