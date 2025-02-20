eea45f4559ed05608c62fae8238f3487f3c17d56
Hugo Sainte-Marie <hugo.saintemarie@gmail.com>
2025-02-20 20:04:26 +0100

    Tweak + fix text-link for dark mode

--- resources/js/components/text-link.tsx
+++ resources/js/components/text-link.tsx
@@ -7,7 +7,10 @@ type LinkProps = ComponentProps<typeof Link>;
 export default function TextLink({ className = '', children, ...props }: LinkProps) {
     return (
         <Link
-            className={cn('underline decoration-neutral-400 underline-offset-2 duration-300 ease-out hover:decoration-neutral-700', className)}
+            className={cn(
+                'text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500',
+                className,
+            )}
             {...props}
         >
             {children}
