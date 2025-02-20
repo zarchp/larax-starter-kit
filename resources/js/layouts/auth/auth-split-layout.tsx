739201fbf8cbfbe40f3ddb9d1a4f737e0c80b495
Hugo Sainte-Marie <hugo.saintemarie@gmail.com>
2025-02-20 20:10:44 +0100

    Bigger Laravel logo and no app name in split layout

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -9,15 +9,14 @@ interface AuthLayoutProps {
 }
 
 export default function AuthSplitLayout({ children, title, description }: AuthLayoutProps) {
-    const { name, quote } = usePage<SharedData>().props;
+    const { quote } = usePage<SharedData>().props;
 
     return (
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
                 <div className="absolute inset-0 bg-zinc-900" />
                 <Link href={route('home')} className="relative z-20 flex items-center text-lg font-medium">
-                    <AppLogoIcon className="mr-2 h-6 fill-current text-white" />
-                    {name}
+                    <AppLogoIcon className="mr-2 size-10 fill-current text-white" />
                 </Link>
                 {quote && (
                     <div className="relative z-20 mt-auto">
