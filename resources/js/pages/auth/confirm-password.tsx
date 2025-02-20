867a0f1822b44cdc4be8a737429313ab5dd9d107
Hugo Sainte-Marie <hugo.saintemarie@gmail.com>
2025-02-20 20:04:43 +0100

    Add label on confirm-password

--- resources/js/pages/auth/confirm-password.tsx
+++ resources/js/pages/auth/confirm-password.tsx
@@ -6,6 +6,7 @@ import { FormEventHandler } from 'react';
 import InputError from '@/components/input-error';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
 
 export default function ConfirmPassword() {
@@ -31,6 +32,7 @@ export default function ConfirmPassword() {
             <form onSubmit={submit}>
                 <div className="space-y-6">
                     <div className="grid gap-2">
+                        <Label htmlFor="password">Password</Label>
                         <Input
                             id="password"
                             type="password"
