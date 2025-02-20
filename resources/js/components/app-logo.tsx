7152319959940b16293be1964dbbd453129cd831
Hugo Sainte-Marie <hugo.saintemarie@gmail.com>
2025-02-20 20:07:52 +0100

    Tweak app logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -4,11 +4,10 @@ export default function AppLogo() {
     return (
         <>
             <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                <AppLogoIcon className="size-[1.375rem] fill-current text-white dark:text-black" />
             </div>
-            <div className="grid flex-1 text-left text-sm leading-tight">
-                <span className="mb-0.5 truncate leading-none font-semibold">Laravel</span>
-                <span className="truncate text-[11px] leading-none tracking-tight opacity-80">Starter Kit</span>
+            <div className="ml-1 grid flex-1 text-left text-sm">
+                <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
             </div>
         </>
     );
