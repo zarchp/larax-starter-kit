bf56775ceac2ccd1b20601b60447bc8a80d3aa53
Hugo Sainte-Marie <hugo.saintemarie@gmail.com>
2025-02-20 20:13:36 +0100

    Same font size in split layout as simple layout

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -33,7 +33,7 @@ export default function AuthSplitLayout({ children, title, description }: AuthLa
                         <AppLogoIcon className="h-10 fill-current text-black sm:h-12" />
                     </Link>
                     <div className="flex flex-col items-start gap-2 text-left sm:items-center sm:text-center">
-                        <h1 className="text-2xl font-bold">{title}</h1>
+                        <h1 className="text-xl font-medium">{title}</h1>
                         <p className="text-muted-foreground text-sm text-balance">{description}</p>
                     </div>
                     {children}
