8e334950749f971b1549fb3c1e6c1f0495ea0057
Hugo Sainte-Marie <hugo.saintemarie@gmail.com>
2025-02-20 20:11:59 +0100

    No separator below Heading, tweak spacing and font size

--- resources/js/components/heading.tsx
+++ resources/js/components/heading.tsx
@@ -1,13 +1,10 @@
-import { Separator } from '@/components/ui/separator';
-
 export default function Heading({ title, description }: { title: string; description?: string }) {
     return (
         <>
-            <div className="space-y-0.5">
-                <h2 className="text-base font-semibold tracking-tight sm:text-lg lg:text-xl">{title}</h2>
-                {description && <p className="text-muted-foreground text-xs md:text-sm">{description}</p>}
+            <div className="mb-8 space-y-0.5">
+                <h2 className="text-xl font-semibold tracking-tight">{title}</h2>
+                {description && <p className="text-muted-foreground text-sm">{description}</p>}
             </div>
-            <Separator className="my-6" />
         </>
     );
 }
