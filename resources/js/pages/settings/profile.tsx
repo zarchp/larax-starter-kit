c3882ce5c17c66fa942d7c6a1d946d7bf84b6aa9
Taylor Otwell <taylor@laravel.com>
2025-02-25 11:36:11 -0700

    fix verification

--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -79,15 +79,15 @@ export default function Profile({ mustVerifyEmail, status }: { mustVerifyEmail: 
 
                         {mustVerifyEmail && auth.user.email_verified_at === null && (
                             <div>
-                                <p className="mt-2 text-sm text-neutral-800">
-                                    Your email address is unverified.
+                                <p className="-mt-4 text-sm text-muted-foreground">
+                                    Your email address is unverified.{' '}
                                     <Link
                                         href={route('verification.send')}
                                         method="post"
                                         as="button"
-                                        className="rounded-md text-sm text-neutral-600 underline hover:text-neutral-900 focus:ring-2 focus:ring-offset-2 focus:outline-hidden"
+                                        className="hover:decoration-current! text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out dark:decoration-neutral-500"
                                     >
-                                        Click here to re-send the verification email.
+                                        Click here to resend the verification email.
                                     </Link>
                                 </p>
 
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
