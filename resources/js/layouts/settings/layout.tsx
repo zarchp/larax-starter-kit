f0fd85f2d6948cb373bbf0d37b62ed8af4b40b62
Ninja <yazjallad@gmail.com>
2025-04-01 08:21:29 -0700

    Update layout.tsx (#86)
    Currently the key is duplicated since this key is also used in the breadcrumbs.
    
    added an index to the key to make it unique so that it will not collide and through a render error for duplicate keys.

--- resources/js/layouts/settings/layout.tsx
+++ resources/js/layouts/settings/layout.tsx
@@ -39,9 +39,9 @@ export default function SettingsLayout({ children }: PropsWithChildren) {
             <div className="flex flex-col space-y-8 lg:flex-row lg:space-y-0 lg:space-x-12">
                 <aside className="w-full max-w-xl lg:w-48">
                     <nav className="flex flex-col space-y-1 space-x-0">
-                        {sidebarNavItems.map((item) => (
+                        {sidebarNavItems.map((item, index) => (
                             <Button
-                                key={item.href}
+                                key={`${item.href}-${index}`}
                                 size="sm"
                                 variant="ghost"
                                 asChild
