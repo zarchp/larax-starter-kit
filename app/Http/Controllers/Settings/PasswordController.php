787515725d6f4d1609c1b387a100989afbfe1fba
Taylor Otwell <taylor@laravel.com>
2025-04-08 22:17:33 -0500

    remove import

--- app/Http/Controllers/Settings/PasswordController.php
+++ app/Http/Controllers/Settings/PasswordController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Settings;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
